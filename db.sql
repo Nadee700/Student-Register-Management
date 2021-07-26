c57707e6abb585805562aa538ca5b4275787d7a9
Nadee <yelka.nadee@gmail.com>
2021-07-26 16:39:22 +0530

    Updated db file

---
+++ db.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[FirstName]
+      ,[LastName]
+      ,[PhoneNo]
+      ,[Email]
+      ,[NIC]
+      ,[DateOfBirth]
+      ,[Address]
+      ,[Avatar]
+  FROM [StudentManagementDB].[dbo].[StudentTable]
