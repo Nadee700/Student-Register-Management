74eb6b700e1778badbe3231e6a776f9d8fab1717
Nadee <yelka.nadee@gmail.com>
2021-07-26 10:07:34 +0530

    Added backed - Asp.net core web API

---
+++ StudentManagement - backend/StudentManagementAPI/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string NIC { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+    }
+}
---
+++ StudentManagement - backend/StudentManagementAPI/Models/StudentDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementAPI.Models
+{
+    public class StudentDbContext : DbContext
+    {
+         
+        public DbSet<Student> Student { get; set; }
+
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("StudentTable");
+        }
+    }
+}
+
